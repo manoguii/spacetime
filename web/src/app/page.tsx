d1ed200e4b1098e3139c9c38892e6e925eff2a9f
guilherme david <guilhermedavidrk@gmail.com>
2023-05-15 21:48:11 -0300

    :memo: config: create web structure

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-zinc-900">
+      <h1 className="text-2xl text-zinc-50">Hello world!</h1>
+    </div>
+  )
+}
